0a0dfb32463ad272d7da4033daa5f99f30cf01c7
APATOUT <APATOUT@intechS1.fr>
2017-12-12 16:02:41 +0100

    12/12/17

---
+++ image.js
@@ -0,0 +1,31 @@
+"use strict"
+
+var im1 = require('/"grass".png');
+var im2 = require('/mario".png');
+var im3 = require('/mur".png');
+var im4 = require('/plaquepression16x16".png');
+var im5 = require('/door".png);
+
+for ( x         ) {
+
+	for ( y     ) {
+
+		if (xy-type === m ) { 
+			marqueur[x":"y] = im3 ;
+
+			
+		} else if (xy-type === v  ) { 
+			marqueur[x":"y] = im1 ;
+	
+		} else if (xy-type === j  ) { 
+			marqueur[x":"y] = im2 ;
+		
+		} else if (xy-type === pp  ) { 
+			marqueur[x":"y] = im4 ;
+	
+		} else if (xy-type === p  ) { 
+			marqueur[x":"y] = im5 ;
+
+		
+		
+			
