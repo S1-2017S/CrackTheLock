4a2bd94846e21e1f048e14033d6e476cb4c67b63
GRANDIN <GRANDIN@intechS1.fr>
2018-01-12 10:49:32 +0100

    push by makefile

---
+++ req_jouer.js
@@ -0,0 +1,38 @@
+//Affiche la maps selection avec différent niveaux de difficulté.
+"use strict";
+
+var fs = require("fs");
+require('remedial');
+var conv = require("./image.js");
+
+var trait = function (req, res, query) {
+	
+	var marqueurs;
+	var page;
+	var map;
+	var niv;
+
+	niv = "5-2";
+
+	map = JSON.parse(fs.readFileSync("map_" + niv + ".json","UTF-8"));
+
+	map.lv = 0;
+
+	page = fs.readFileSync('jeu.html', 'utf-8');
+
+	marqueurs = {};
+
+	fs.writeFileSync("map_" + query.pseudo + ".json",JSON.stringify(map),"UTF-8");
+
+	marqueurs = conv(map);
+	marqueurs.pseudo = query.pseudo;
+	marqueurs.niv = niv;
+	page = page.supplant(marqueurs);
+	console.log(marqueurs.pseudo +" "+ query.pseudo);
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(page);
+	res.end();
+
+};
+
+module.exports = trait;
